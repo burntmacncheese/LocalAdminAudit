3689c2156b241781ae19b8ca1a674c402e80238e
John Weise <31836935+burntmacncheese@users.noreply.github.com>
2018-01-23 18:42:45 -0500

    Add files via upload

---
+++ Localadmin.ps1
@@ -0,0 +1,13 @@
+﻿
+
+invoke-command {
+$members = net localgroup administrators | 
+ where {$_ -AND $_ -notmatch "command completed successfully"} | 
+ select -skip 4
+New-Object PSObject -Property @{
+ Computername = $env:COMPUTERNAME
+ Group = "Administrators"
+ Members=$members
+ }
+} -computername (Get-ADComputer -Filter * -SearchBase "$searchbase" | Select-Object -expand Name)  -HideComputerName | 
+Select * -ExcludeProperty RunspaceID | Select-Object Computername,Group,Members | Sort-Object computername | Export-Csv $path
